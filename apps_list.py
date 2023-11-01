a469576ab8c946a31f11c3448012f42264fa922a
JaspreetKaur0410 <kaur.jasp@northeastern.edu>
2023-11-01 16:38:16 -0400

    all_dashboards

---
+++ apps_list.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+st.set_page_config(layout="wide")
+st.header("Dashboard Apps")
+st.write("created by Jaspreet Kaur")
+
+col1_head,col2_head,col3_head=st.columns(3)
+col1,col2,col3=st.columns(3)
+col4,col5,col6=st.columns(3)
+
+
+with col1_head:
+    st.subheader("Name of Project")
+
+with col2_head:
+    st.subheader("Description")
+
+with col3_head:
+    st.subheader("Link to the Project")
+
+with col1:
+    st.info("sales_analytics_dashboard")
+
+with col2:
+    st.info("sales_analytics_dashboard is built using streamlit and contain filters based on which analytics are generated")
+
+with col3:
+    st.link_button("sales_analytics_dashboard", "https://salesanalyticsdashboard-nddhpybwhoaerrswpp8rax.streamlit.app/")
+    
+with col4:
+    st.info("uber_pickups")
+
+with col5:
+    st.info("uber_pickups is built using streamlit and analyze about timings when people book more cabs")
+
+with col6:
+    st.link_button("sales_analytics_dashboard", "https://yt6hwgasmti9kt4vfs4jiu.streamlit.app/")
+
+
